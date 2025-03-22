fe0010e95c5e58f5f4f9c915985c18b8972d9554
m2ri16 <00000845849.marina@unicap.br>
2025-03-22 15:42:04 -0300

    Exemplo 1

---
+++ Exemplo.c
@@ -0,0 +1,12 @@
+// definição do tipo
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+    } data;
+
+// declaração de variável
+data hoje;
+hoje.dia = 25;
+hoje.mes = 08;
+hoje.ano = 2022;
