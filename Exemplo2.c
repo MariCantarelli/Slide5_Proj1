1a3b390f9e60e3dae5b3d65bfee5aa193e1f479d
m2ri16 <00000845849.marina@unicap.br>
2025-03-22 15:50:44 -0300

    Exemplo 3

--- Exemplo2.c
+++ Exemplo2.c
@@ -1,4 +1,16 @@
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+    } data;
+
+
 data *p; // p é um ponteiro para registros do tipo data
 data hoje;
 p = &hoje; // agora p aponta para hoje
 (*p).dia = 31; // mesmo efeito que hoje.dia = 31
+
+printf("hoje.dia: %d\n", hoje.dia);
+printf("hoje.mes: %d\n", hoje.mes);
+printf("(*p).dia: %d\n", (*p).dia);
+printf("p->dia: %d\n", p->dia);
